6763eabed333eef680d304c64bcddf2c5ae9ed12
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-01 12:21:11 +0930

    add seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+insert into department(name)
+values ('Engineering'),
+       ('Accounting'),
+       ('Marketing'),
+       ('Sales');
+       
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("software engineer", 120000, 1),
+    ("project manager", 90000, 1),
+    ("engineering manager", 225000, 1),
+    ("accountant", 70000, 2),
+    ("accounting manager", 120000, 2),
+    ("product marketing manager", 50000, 3),
+    ("marketing lead", 150000, 3),
+    ("sales rep", 85000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jurgen", "Klopp", 1, 1 ),
+       ("Jordan", "Henderson", 2, 1),
+       ("Virgil", "Van Dyke", 3, 14),
+       ("Allison", "Becker", 4, 3),
+       ("Mohamed", "Salah", 5, 14),
+       ("Sadio", "Mane", 6, 5),
+       ("Diego", "Jota", 7, 14),
+       ("Roberto", "Firmino", 8, 7);
