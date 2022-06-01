88f9e27ea447533ab74f8588268b46014446fa06
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-01 10:37:11 +0930

    add prompt consisting of the the questions asked to the user and check if the selction is being made

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 require("dotenv").config();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquier = require("inquirer");
 const cTable = require("console.table");
 
-// set connection to the database
+// create connection to the database
 const db = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "employee_DB",
 });
+
+//connect to the database
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("connected to employee_db");
+
+  runPrompt();
+});
+
+// function to run the starting questions prompt
+
+function runPrompt() {
+  inquier
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View Department",
+        "View role",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Role",
+        "Quit",
+      ],
+    })
+    .then(function (selection) {
+      console.log(selection);
+    });
+}
